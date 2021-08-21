98d93f876d3585c16d8594e103b0dfabff908d53
Penny <88145246+nnype@users.noreply.github.com>
2021-08-21 11:22:55 +0300

    Add files via upload

---
+++ Lexical Analysis/lexer_Pasc600.h
@@ -0,0 +1,62 @@
+#define T_EOF 0
+
+// KEYWORDS
+#define T_PROGRAM 1
+#define T_CONST 2
+#define T_TYPE 3
+#define T_ARRAY 4
+#define T_OF 51
+#define T_VAR 5
+#define T_FORWARD 6
+#define T_FUNCTION 7
+#define T_PROCEDURE 8
+#define T_INTEGER 9
+#define T_REAL 10
+#define T_BOOLEAN 11
+#define T_CHAR 12
+#define T_STRING 13
+#define T_BEGIN 14
+#define T_END 15
+#define T_IF 16
+#define T_THEN 17
+#define T_ELSE 18
+#define T_CASE 19
+#define T_OTHERWISE 20
+#define T_WHILE 21
+#define T_DO 22
+#define T_FOR 23
+#define T_DOWNTO 24
+#define T_TO 25
+#define T_READ 26
+#define T_WRITE 27
+#define T_LENGTH 28
+
+// IDENTIFIERS(VARIABLES / FUNCTION NAMES)
+#define T_ID 29
+
+//CONSTANTS
+#define T_ICONST 30
+#define T_RCONST 31
+#define T_BCONST 32
+#define T_CCONST 33
+#define T_SCONST 34
+
+// OPERATORS
+#define T_RELOP 35
+#define T_ADDOP 36
+#define T_OROP 37
+#define T_MULDIVANDOP 38
+#define T_NOTOP 39
+
+// SYMBOLSS
+#define T_LPAREN 40
+#define T_RPAREN 41
+#define T_SEMI 42
+#define T_DOT 43
+#define T_COMMA 44
+#define T_EQU 45
+#define T_COLON 46
+#define T_LBRACK 47
+#define T_RBRACK 48
+#define T_ASSIGN 49
+#define T_DOTDOT 50 
